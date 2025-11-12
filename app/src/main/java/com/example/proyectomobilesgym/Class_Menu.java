0c18500a129d101cb1dce9dd488e26721449245d
Anthony Conejo <twonejo@gmail.com>
2025-11-11 20:03:47 -0600

    creacion de view listMenu
    se creo la clase de menu , su adapter y su parte grafica segun es necesario para el menu

---
+++ app/src/main/java/com/example/proyectomobilesgym/Class_Menu.java
@@ -0,0 +1,42 @@
+package com.example.proyectomobilesgym;
+
+public class Class_Menu {
+    public int id;
+    public int imagen;
+    public String txtPrincipal;
+
+
+    public Class_Menu(int id, int imagen, String txtPrincipal) {
+        this.id = id;
+        this.imagen = imagen;
+        this.txtPrincipal = txtPrincipal;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getTxtPrincipal() {
+        return txtPrincipal;
+    }
+
+    public void setTxtPrincipal(String txtPrincipal) {
+        this.txtPrincipal = txtPrincipal;
+    }
+
+
+}
+
